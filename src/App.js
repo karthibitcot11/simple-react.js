eb2027c539659684b14162c4cdd15ab8bc631471
karthibitcot11 <karthikeyanm01196@gmail.com>
2025-02-22 16:54:34 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          pr request approved !!!
         </p>
         <a
           className="App-link"
